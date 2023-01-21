fdc8cb91d9997d531666359f81370d6c34be0d22
Bence Konnyu <74246474+londony11@users.noreply.github.com>
2023-01-21 14:29:24 +0100

    Example data for script

---
+++ data_sample.py
@@ -0,0 +1,19 @@
+RECIPIENTS = ["example@email.com"]
+MY_EMAIL = "myemail@email.com"
+APP_PASSWORD = "password_for_myeamil"
+OTP_BOOK_DATA = [
+    {
+        "tulajdonos": "First Last",
+        "sorozat": "12",
+        "sorszám": "1234567",
+        "címlet": "100 Ft",
+    },
+    {
+        "tulajdonos": "First2 Last2",
+        "sorozat": "12",
+        "sorszám": "1234567",
+        "címlet": "100 Ft",
+    },
+]
+
+POSTSCRIPT = "U.I: Utóirat"
