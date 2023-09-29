c670bcfc70c02e8beef3a30783359da7ba85dee0
londony11 <londony11@gmail.com>
2023-09-29 07:17:02 +0200

    restructure to OOP

--- data_sample.py
+++ data_sample.py
@@ -1,18 +1,33 @@
+URL = "https://www.otpbank.hu/portal/hu/Megtakaritas/ForintBetetek/Gepkocsinyeremeny"
+DATETIME_TRANSLATE = {
+    "January": "Január",
+    "February": "Február",
+    "March": "Március",
+    "April": "Április",
+    "May": "Márjus",
+    "June": "Június",
+    "July": "Július",
+    "August": "Augusztus",
+    "September": "Szeptember",
+    "October": "Október",
+    "November": "November",
+    "December": "December",
+}
 RECIPIENTS = ["example@email.com"]
 MY_EMAIL = "myemail@email.com"
 APP_PASSWORD = "password_for_myeamil"
 OTP_BOOK_DATA = [
     {
-        "tulajdonos": "First Last",
-        "sorozat": "12",
-        "sorszám": "1234567",
-        "címlet": "100 Ft",
+        "owner": "First Last",
+        "series": "12",
+        "number": "1234567",
+        "value": "100 Ft",
     },
     {
-        "tulajdonos": "First2 Last2",
-        "sorozat": "12",
-        "sorszám": "1234567",
-        "címlet": "100 Ft",
+        "owner": "First2 Last2",
+        "series": "12",
+        "number": "1234567",
+        "value": "100 Ft",
     },
 ]
 
